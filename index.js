2f4f367459a332b3e307b3a8b86a9635feed8dc8
Sumit Patel <sumitpatel0147@gmail.com>
2020-11-25 22:39:47 -0500

    adding more stuff to the username in MAP

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ app.get('/',(req, res) => {
 app.listen(process.env.PORT || 3000)
 
 let passwords = new Map()
-passwords.set('bob@gmail.com','pwd')
+passwords.set('sumit@gmail.com','pwd')
+passwords.set('bob@gmail.com','hello')
 app.get('/login', (req,res) => {
     if(passwords.get(req.query.username) === req.query.password){
         console.log("if statement 1", passwords.get(req.query.username))
