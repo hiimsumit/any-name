089ae19ded8ff552c5a0fc769d20f8681c49699b
Sumit Patel <sumitpatel0147@gmail.com>
2020-11-25 19:26:41 -0500

    added slash login with email and password

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ app.get('/',(req, res) => {
 })
 
 app.listen(process.env.PORT || 3000)
+
+let passwords = new Map()
+passwords.set('sumit@gmail.com','pwd')
+app.get('/login', (req,res) => {
+    if(passwords.get(req.query.username) === req.query.password){
+        res.send("success")
+    }else{
+        res.send("login failed")
+    }
+})
