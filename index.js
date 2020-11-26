a21b9560e3aaaf061b3bfcd72987500af8b39906
Sumit Patel <sumitpatel0147@gmail.com>
2020-11-25 19:34:11 -0500

    syntax error fixed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let passwords = new Map()
 passwords.set('sumit@gmail.com','pwd')
 app.get('/login', (req,res) => {
     if(passwords.get(req.query.username) === req.query.password){
-        console.log('passwords.get(req.query.username',passwords.get(req.query.username))\]
+        console.log('passwords.get(req.query.username',passwords.get(req.query.username))
         console.log('req.query.password',req.query.password)
         res.send("success")
     }else{
