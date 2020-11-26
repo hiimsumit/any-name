19a1d1a88dc7020b623d59c086b732f5954bb66e
Sumit Patel <sumitpatel0147@gmail.com>
2020-11-25 19:31:05 -0500

    added console logs to test the value

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ let passwords = new Map()
 passwords.set('sumit@gmail.com','pwd')
 app.get('/login', (req,res) => {
     if(passwords.get(req.query.username) === req.query.password){
+        console.log('passwords.get(req.query.username',passwords.get(req.query.username))\]
+        console.log('req.query.password',req.query.password)
         res.send("success")
     }else{
         res.send("login failed")
