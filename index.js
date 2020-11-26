c829ff4f1226496527492f1a3c879f4a92969901
Sumit Patel <sumitpatel0147@gmail.com>
2020-11-25 20:01:51 -0500

    added proper res.send

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ let passwords = new Map()
 passwords.set('sumit@gmail.com','pwd')
 app.get('/login', (req,res) => {
     if(passwords.get(req.query.username) === req.query.password){
-        console.log('testing',"testing first console")
-        console.log('testing2','testing the second console log')
+        res.send(passwords.get(req.query.username))
+        res.send(req.query.password)
         res.send("success")
     }else{
         res.send("login failed")
