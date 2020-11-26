41f883e865de7aeb5357f276627f02735ba1820b
Sumit Patel <sumitpatel0147@gmail.com>
2020-11-25 20:39:46 -0500

    changed the console log message

--- index.js
+++ index.js
@@ -11,12 +11,10 @@ let passwords = new Map()
 passwords.set('sumit@gmail.com','pwd')
 app.get('/login', (req,res) => {
     if(passwords.get(req.query.username) === req.query.password){
-        res.send(passwords.get(req.query.username))
-        res.send(req.query.password)
+        console.log('testing','testing fourth console log')
         res.send("success")
     }else{
         res.send("login failed")
-        console.log('testing3',"testing third console")
-        console.log('testing4','testing fourth console log')
+        console.log('testing4','testing in login fail')
     }
 })
